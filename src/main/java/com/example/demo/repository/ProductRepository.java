eeea27321e8725196de335b5b0c8a1b3eacb30b0
ToFatToBat <kpapiernia@wp.pl>
2020-08-27 09:11:18 +0200

    .

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class ProductRepository {
     private List<Product> products;
 
